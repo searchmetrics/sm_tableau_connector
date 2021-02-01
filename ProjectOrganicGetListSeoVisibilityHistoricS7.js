73e8bc72e20e3b41698a67029da119ca10973d8f
fernanda-alves-sm <32565123+fernanda-alves-sm@users.noreply.github.com>
2021-02-01 15:48:31 +0100

    Added historicalProject and Research
    Added historicalProject and Research

---
+++ ProjectOrganicGetListSeoVisibilityHistoricS7.js
@@ -0,0 +1,44 @@
+function getProjectOrganicGetListSeoVisibilityHistoricS7Schema(){ 
+	return {
+		id: "SeoVisibilityHistoric",
+		alias: "Seo Visibility Historic",
+		columns: [
+			{
+				id: "date",
+				alias: "date",
+				dataType: tableau.dataTypeEnum.string
+			},
+			{
+				id: "domain",
+				alias: "domain",
+				dataType: tableau.dataTypeEnum.string
+			},
+			{
+				id: "sum_visibility",
+				alias: "visibility",
+				dataType: tableau.dataTypeEnum.int
+			},
+			{
+				id: "count_keyword",
+				alias: "count_kewyord",
+				dataType: tableau.dataTypeEnum.int
+			}
+		]
+	};
+}
+	
+function transformProjectOrganicGetListSeoVisibilityHistoricData(data){
+	var collectedData = [];
+
+	data.response.map(function(el){
+		collectedData.push({
+			"date": el.date,
+			"domain": el.data[0].domain,
+			"sum_visibility": el.data[0].sum_visibility,
+			"count_keywords": el.data[0].count_keywords 
+		});
+	});
+
+	return collectedData;
+}
+
